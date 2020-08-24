dc3a6046f504399bb2a0ce4753fcc7db99895ba0
Deshon <deshonjgrimes@gmail.com>
2020-08-24 10:38:13 -0500

    Initial Commit

---
+++ tabletalk/src/test/java/com/shongrimes/tabletalk/TabletalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shongrimes.tabletalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabletalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
